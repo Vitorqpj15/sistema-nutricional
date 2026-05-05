64cf50f20f27efa07ec5f5689afe926c6df768e2
Vitorqpj15 <queirozvitor@discente.ufg.br>
2026-05-05 16:13:47 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,15 @@ const PORT = 3000;
 app.use(cors());       
 app.use(express.json()); 
 
+app.get('/ping', (req, res) => res.send('pong'));
+
 app.use('/alimentos', alimentosRoutes);
 
+process.on('uncaughtException', (err) => {
+    console.error('❌ ERRO CRÍTICO (O servidor ia cair):', err);
+});
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`🚀 Servidor ATIVO em http://localhost:${PORT}`);
+    console.log('Pressione CTRL+C para parar');
 });
