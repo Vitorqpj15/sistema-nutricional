b0c5d568f6708ec98e98d094b3c6587efcd0cc47
Vitorqpj15 <queirozvitor@discente.ufg.br>
2026-05-06 16:07:54 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,19 @@ const cors = require('cors');
 require('dotenv').config();
 
 const alimentosRoutes = require('./routes/alimentos');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());       
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
-app.get('/ping', (req, res) => res.send('pong'));
-
+// Rotas
 app.use('/alimentos', alimentosRoutes);
-
-process.on('uncaughtException', (err) => {
-    console.error('❌ ERRO CRÍTICO (O servidor ia cair):', err);
-});
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor ATIVO em http://localhost:${PORT}`);
-    console.log('Pressione CTRL+C para parar');
+  console.log(`🚀 Servidor ATIVO em http://localhost:${PORT}`);
+  console.log(`Pressione CTRL+C para parar`);
 });
